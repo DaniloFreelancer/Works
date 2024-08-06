e82ddbad344463c92ee73223176f6eae1fde5b87
DaniloFreelancer <144622931+DaniloFreelancer@users.noreply.github.com>
2024-08-06 14:58:56 +0300

    Add files via upload

--- dist04/js/app.js
+++ dist04/js/app.js
@@ -6696,6 +6696,39 @@ PERFORMANCE OF THIS SOFTWARE.
         }
         const da = new DynamicAdapt("max");
         da.init();
+        document.addEventListener("DOMContentLoaded", (function() {
+            const options = {
+                root: null,
+                rootMargin: "0px",
+                threshold: .1
+            };
+            const observer = new IntersectionObserver(((entries, observer) => {
+                entries.forEach((entry => {
+                    if (entry.isIntersecting) {
+                        const element = entry.target;
+                        const text = element.getAttribute("data-text");
+                        const delay = parseInt(element.getAttribute("data-delay")) || 0;
+                        let index = 0;
+                        function typeText() {
+                            if (index < text.length) {
+                                element.textContent += text.charAt(index);
+                                index++;
+                                setTimeout(typeText, 100);
+                            }
+                        }
+                        setTimeout((() => {
+                            element.classList.add("visible");
+                            setTimeout(typeText, 100);
+                        }), delay);
+                        observer.unobserve(element);
+                    }
+                }));
+            }), options);
+            const elements = document.querySelectorAll(".title-type");
+            elements.forEach((element => {
+                observer.observe(element);
+            }));
+        }));
         window["FLS"] = false;
         menuInit();
         formFieldsInit({
